235d4c27df81dbdf7f7a677dfccbd6c75d84b627
Travis75 <travis.gibson75@gmail.com>
2014-10-10 14:42:32 -0700

    changed serve_static_assets to true in production to enable asset pipleline on heroku

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,14 +2,8 @@ $(document).ready(function(){
 
 $('.slider').slick()
 
-$("#blog").hover(
-
-  function(){console.log(this)},
-
-  function(){console.log(this)})
-
-$("#blog").hover(
-
-  function(){$(this).css('border-bottom', '2px solid black')})
-
+$("h4").hover(
+  function(){$(this).css('border-bottom', '2px solid black')},
+  function(){$(this).css('border-bottom', '')}
+  )
 })
