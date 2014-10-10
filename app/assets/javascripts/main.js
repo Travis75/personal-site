bbfc32df71271dd84c4f532a13aa54dc4b97dc6d
Travis75 <travis.gibson75@gmail.com>
2014-10-10 13:40:21 -0700

    updated readme

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
 
 $('.slider').slick()
 
+$("#blog").hover(
+
+  function(){console.log(this)},
+
+  function(){console.log(this)})
+
+$("#blog").hover(
+
+  function(){$(this).css('border-bottom', '2px solid black')})
+
 })
